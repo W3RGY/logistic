288d4231d777092857e9c358247b8c0c6de67368
W3RGY <w3rgy1992@gmail.com>
2022-04-10 19:08:51 +0200

    main

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ $(function () {
 	let header = $("#header");
 	let introH = intro.innerHeight();
 	let headerH = header.innerHeight();
+	let scrollTop = $(window).scrollTop();      /*position on page load*/
 
 	/* Header class on scroll
 	=======================*/
@@ -43,20 +44,32 @@ $(function () {
 
 	/* ScrollSpy
 	==========================*/
+	let windowH = $(window).height();
+	scrollSpy(scrollTop);
 
 	$(window).on("scroll", function () {
+		scrollTop = $(this).scrollTop();
+		scrollSpy(scrollTop);
+	});
 
+	function scrollSpy(scrollTop) {
 		$("[data-scrollspy]").each(function () {
-
 			let $this = $(this);
 			let sectionId = $this.data('scrollspy');
 			let sectionOffset = $this.offset().top;
+			sectionOffset = sectionOffset - (windowH * 0.33333);
 
-			console.log(sectionOffset);
+			if (scrollTop >= sectionOffset) {
+				$('#nav [data-scroll]').removeClass('active');
+
+				$('#nav [data-scroll="' + sectionId + '"]').addClass('active');
+			}
+
+			if (scrollTop == 0) {
+				$('#nav [data-scroll]').removeClass('active');
+			}
 		});
-	});
-
-
+	}
 
 
 
