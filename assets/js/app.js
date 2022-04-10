d3e21e548b4ac15622b45e2c8605503925694da4
W3RGY <w3rgy1992@gmail.com>
2022-04-10 21:03:34 +0200

    main

--- assets/js/app.js
+++ assets/js/app.js
@@ -71,8 +71,50 @@ $(function () {
 		});
 	}
 
+	/* Modal
+	==========================*/
+	$('[data-modal]').on('click', function(event) {
+		event.preventDefault();
+		let modal = $(this).data('modal');
 
+		$('body').addClass('no-scroll');
+		$(modal).addClass('show');
 
+		setTimeout(function() {
+			$(modal).find('.modal__content').css({
+				transform: 'scale(1)',
+				opacity: '1'
+			});
+		});
+	});
+
+	$('[data-modal-close]').on('click', function(event) {
+		event.preventDefault();
+		let modal = $(this).parents('.modal');
+		modalClose(modal);
+	});
+
+	$('.modal').on('click', function () {
+		let modal = $(this);
+		modalClose(modal);
+	});
+
+	$('.modal__content').on('click', function(event) {                       /*does not close the modal content window*/
+		event.stopPropagation();
+	});
+
+	function modalClose(modal) {
+
+		modal.find('.modal__content').css({
+			transform: 'scale(0.5)',
+			opacity: '0'
+		});
+
+		setTimeout(function() {
+			$('body').removeClass('no-scroll');
+			modal.removeClass('show');
+		}, 200);
+	}
 
 });
 
